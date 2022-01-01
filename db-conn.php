1fa52085248f3d384473db5197e3ceb939a5e888
re4nightwing <dulan9595531@gmail.com>
2022-01-01 05:41:24 +0530

    gcp stuff removed

--- db-conn.php
+++ db-conn.php
@@ -1,14 +1,14 @@
 <?php
 
 // Connect to the database
-$usr = getenv('CLOUDSQL_USER');
-$pass = getenv('CLOUDSQL_PASSWORD');
-$inst = getenv('CLOUDSQL_DSN');
-$db = getenv('CLOUDSQL_DB');
+//$usr = getenv('CLOUDSQL_USER');
+//$pass = getenv('CLOUDSQL_PASSWORD');
+//$inst = getenv('CLOUDSQL_DSN');
+//$db = getenv('CLOUDSQL_DB');
 
 try {
-  $dbh = new PDO("mysql:unix_socket=$inst;dbname=$db", $usr, $pass);
-  //$dbh = new PDO("mysql:host=localhost;dbname=slayerscans", 'dulan', 'good');
+  //$dbh = new PDO("mysql:unix_socket=$inst;dbname=$db", $usr, $pass);
+  $dbh = new PDO("mysql:host=35.223.198.221;dbname=ruby_cons_db", 'root', 'A0FCaA9I1Fcp5iuf');
 } catch (PDOException $e) {
   echo "error:  " . $e;
 }
