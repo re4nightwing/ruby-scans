bf6c4837d10e06b58b4d1146a027221f7f6a1c77
re4nightwing <dulan9595531@gmail.com>
2022-01-14 15:05:54 +0530

    credentials changed to new database

--- db-conn.php
+++ db-conn.php
@@ -8,7 +8,7 @@
 
 try {
   //$dbh = new PDO("mysql:unix_socket=$inst;dbname=$db", $usr, $pass);
-  $dbh = new PDO("mysql:host=34.72.85.245;dbname=ruby_cons_db", 'root', '2jBDEgC50DkjjqAj');
+  $dbh = new PDO("mysql:host=35.226.198.122;dbname=ruby_cons_db", 'root', 'FJKHBfNKbB9ieOs3');
 } catch (PDOException $e) {
   echo "error:  " . $e;
 }
