fc1acfad733097ce6503582d36a4cf0643f1d3a2
re4nightwing <dulan9595531@gmail.com>
2022-01-14 15:05:05 +0530

    credentials changed to new database

--- db-conn.php
+++ db-conn.php
@@ -8,6 +8,7 @@ $db = getenv('CLOUDSQL_DB');
 
 try {
   $dbh = new PDO("mysql:unix_socket=$inst;dbname=$db", $usr, $pass);
+  //$dbh = new PDO("mysql:host=35.226.198.122;dbname=ruby_cons_db", 'root', 'FJKHBfNKbB9ieOs3');
   //$dbh = new PDO("mysql:host=localhost;dbname=slayerscans", 'dulan', 'good');
 } catch (PDOException $e) {
   echo "error:  " . $e;
